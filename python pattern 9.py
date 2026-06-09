2904e37dafbe4d3c7ce97851477cef4a84d96951
Kiran Nimbalkar <nimbalkarkiran301@gmail.com>
2026-06-09 21:52:58 +0530

    Add files via upload

---
+++ python pattern 9.py
@@ -0,0 +1,13 @@
+#print
+#  * * * *
+#    * * *
+#      * *
+#        *
+n = 4
+
+for i in range(n):
+    for k in range(i):
+        print("  ", end="")    
+    for j in range(n - i):
+        print("*", end=" ")
+    print()
